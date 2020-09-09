3e8447f38fb5f4411d972dd1782c548a6fd3ca92
Thiago <thiagolima1011@gmail.com>
2020-09-09 14:02:08 -0300

    Atualização do Console

--- TesteIdade.java
+++ TesteIdade.java
@@ -1,25 +1,20 @@
-package br.com.pesquisamercado.teste;
+public class TestaIdade {
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.Scanner;
-
-import br.com.pesquisamercado.modelo.Idade;
-
-public class TesteIdade {
-
-	public static void main(String[] args) throws ParseException {
-
-		Idade idade = new Idade();
-		Scanner scanner = new Scanner(System.in);
-		String dataNascimento;
-		System.out.println("Digite a data de Nascimento: ");
-		dataNascimento = scanner.next();
-		Date date = new SimpleDateFormat("dd/MM/yyyy").parse(dataNascimento);
-		int idade1 = idade.calcularIdade(date);
-		System.out.println(idade1);
-	}
+    public static void main(String[] args) {
+        
+            Scanner scan = new Scanner(System.in);
+            int idade = 0; 
+            
+            Idade id = new Idade();
+            
+            System.out.print("Digite uma data: ");
+            String dataNascimento = scan.nextLine();
+            //System.out.println(dataNascimento.length());
+            idade = id.calcularIdade(dataNascimento);           
+            System.out.println(idade);
+  
+     
+    }
+    
 }
+
