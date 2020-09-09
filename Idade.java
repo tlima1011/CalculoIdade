d215fb9dc90a0e36938ded5131a8954cb2771530
Thiago <thiagolima1011@gmail.com>
2020-09-09 14:01:01 -0300

    Atualização do Modelo de Cálculo de Idade

--- Idade.java
+++ Idade.java
@@ -1,57 +1,48 @@
-package br.com.pesquisamercado.modelo;
+package br.com.testethiago.modelo;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.Month;
-import java.time.Period;
-import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
-import java.time.temporal.TemporalAccessor;
-import java.time.temporal.TemporalQuery;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
-import java.util.Locale;
-import java.util.Scanner;
 
+/**
+ *
+ * @author Thiago
+ */
 public class Idade {
 
-	private int idade;
+    public String anoNascimento;
+    public Date dataNascimento;
+    public int anoAtual;
+    public int idade;
 
-	public int getIdade() {
-		return idade;
-	}
+    public static int calcularIdade(String dataNasc) {
 
-	public void setIdade(int idade) {
-		this.idade = idade;
-	}
+        Calendar calendario = GregorianCalendar.getInstance();
 
-	public int calcularIdade(Date dataNascimento) {
-		GregorianCalendar dataHoje = new GregorianCalendar();
-		int diaAtual = 0, mesAtual = 0, anoAtual = 0; 
-		diaAtual = dataHoje.get(Calendar.DAY_OF_MONTH);
-		mesAtual = dataHoje.get(Calendar.MONTH) + 1;
-		anoAtual = dataHoje.get(Calendar.YEAR);
-		SimpleDateFormat formatador = new SimpleDateFormat("dd/MM/yyyy");
-		String dtNasc = formatador.format(dataNascimento);
-		String diaNasc = dtNasc.substring(0, 2);
-		String mesNasc = dtNasc.substring(3, 5);
-		String anoNasc = dtNasc.substring(6, 10);
-		int diaNascimento = Integer.parseInt(diaNasc);
-		int mesNascimento = Integer.parseInt(mesNasc);
-		int anoNascimento = Integer.parseInt(anoNasc);
-		idade = anoAtual - anoNascimento;
-		if(mesAtual != mesNascimento) {
-			if(mesAtual < mesNascimento) {
-				idade--; 
-				return idade; 
-			}
-		}
-		else {
-			return idade;
-		}
-		return idade;
-	}
+        int diaAtual = calendario.get(Calendar.DAY_OF_MONTH);
+        int mesAtual = calendario.get(Calendar.MONTH) + 1;
+        int anoAtual = calendario.get(Calendar.YEAR);
+        int idade = 0;
+
+        String dia = dataNasc.substring(0, 2);
+        String mes = dataNasc.substring(3, 5);
+        String ano = dataNasc.substring(6, 10);
+
+        int diaNascimento = Integer.parseInt(dia);
+        int mesNascimento = Integer.parseInt(mes);
+        int anoNascimento = Integer.parseInt(ano);
+        
+        idade = anoAtual - anoNascimento;
+        if (mesAtual > mesNascimento) {
+            idade = anoAtual - anoNascimento;
+        } else if(diaAtual >= diaNascimento){
+            idade = anoAtual - anoNascimento;
+        }else{
+            idade--;
+        }
+        return idade;
+    }
 }
