a1d4060aec6858eb97546b5a5fcc3a41163f8ee2
Thiago <thiagolima1011@gmail.com>
2021-08-19 11:19:03 -0300

    Código Mais simplificado com cálculo de idade

--- Idade.java
+++ Idade.java
@@ -12,37 +12,15 @@ import java.util.GregorianCalendar;
  * @author Thiago
  */
 public class Idade {
+    
+    public static int calcularIdade(Date dataNascimento) {
 
-    public String anoNascimento;
-    public Date dataNascimento;
-    public int anoAtual;
-    public int idade;
-
-    public static int calcularIdade(String dataNasc) {
-
-        Calendar calendario = GregorianCalendar.getInstance();
-
-        int diaAtual = calendario.get(Calendar.DAY_OF_MONTH);
-        int mesAtual = calendario.get(Calendar.MONTH) + 1;
-        int anoAtual = calendario.get(Calendar.YEAR);
-        int idade = 0;
-
-        String dia = dataNasc.substring(0, 2);
-        String mes = dataNasc.substring(3, 5);
-        String ano = dataNasc.substring(6, 10);
-
-        int diaNascimento = Integer.parseInt(dia);
-        int mesNascimento = Integer.parseInt(mes);
-        int anoNascimento = Integer.parseInt(ano);
-        
-        idade = anoAtual - anoNascimento;
-        if (mesAtual > mesNascimento) {
-            idade = anoAtual - anoNascimento;
-        } else if(diaAtual >= diaNascimento){
-            idade = anoAtual - anoNascimento;
-        }else{
-            idade--;
-        }
-        return idade;
-    }
+		int idade = 0;
+		Date dataHoje = new Date();
+		long diffEmMil = Math.abs(dataHoje.getTime() - dataNascimento.getTime());
+		long diff = TimeUnit.DAYS.convert(diffEmMil, TimeUnit.MILLISECONDS);
+		idade = (int) (diffEmMil / (1000 * 60 * 60 * 24) / 365);
+		return idade;
+	}
+    
 }
