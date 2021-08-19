b0b1c4412f56cf67d1bc42fb1899ec48e99b7195
Thiago <thiagolima1011@gmail.com>
2021-08-19 11:23:52 -0300

    Identação da Main

--- TesteIdade.java
+++ TesteIdade.java
@@ -4,14 +4,15 @@ public class TestaIdade {
         
            Scanner sc = new Scanner(System.in);
 
-		   SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	   SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-		    try {
-			    System.out.print("Informe a Data de Nascimento: ");
+	    try {
+		System.out.print("Informe a Data de Nascimento: ");
                 String nascimento = sc.nextLine();
                 Date dataNascimento = sdf.parse(nascimento);
                 System.out.println("Idade: " + Idade.calcularIdade(dataNascimento) + " anos.");
-            } catch (ParseException e) {
+            } 
+	    catch (ParseException e) {
                 System.out.println("Formato de Data inválida (dd/MM/yyyy): " + e.getMessage());
             }
             sc.close();
